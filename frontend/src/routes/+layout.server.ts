381d32fec30378f9b985b91961cefb5621cd3588
rayanry9 <rayanhldr@gmail.com>
2025-10-16 13:27:30 +0530

    fix: starting page

--- frontend/src/routes/+layout.server.ts
+++ frontend/src/routes/+layout.server.ts
@@ -2,15 +2,17 @@ import { PUBLIC_API_URL } from '$env/static/public';
 import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
-export const load: LayoutServerLoad = async ({ fetch }) => {
-	const res = await fetch(PUBLIC_API_URL + '/getUser', { method: 'GET', credentials: 'include' });
-	if (res.status != 200) {
-		fetch(PUBLIC_API_URL + '/logout', { method: 'POST', credentials: 'include' });
-		redirect(301, '/login');
+export const load: LayoutServerLoad = async ({ fetch, url }) => {
+	if (url.pathname != '/login') {
+		const res = await fetch(PUBLIC_API_URL + '/getUser', { method: 'GET', credentials: 'include' });
+		if (res.status != 200) {
+			fetch(PUBLIC_API_URL + '/logout', { method: 'POST', credentials: 'include' });
+			redirect(301, '/login');
+		}
+		let userData = await res.json();
+		userData = userData['data'];
+		return {
+			user: userData
+		};
 	}
-	let userData = await res.json();
-	userData = userData['data'];
-	return {
-		user: userData
-	};
 };
