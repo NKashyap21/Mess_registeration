7b96831bcc332d5c34fcd139e39a615c5ef74cff
rayanry9 <rayanhldr@gmail.com>
2025-10-17 22:46:27 +0530

    fix: prolly for layout"

--- frontend/src/routes/+layout.server.ts
+++ frontend/src/routes/+layout.server.ts
@@ -4,15 +4,23 @@ import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ fetch, url }) => {
 	if (url.pathname != '/login') {
-		const res = await fetch(PUBLIC_API_URL + '/getUser', { method: 'GET', credentials: 'include' });
-		if (res.status != 200) {
-			fetch(PUBLIC_API_URL + '/logout', { method: 'POST', credentials: 'include' });
+		try {
+			const res = await fetch(PUBLIC_API_URL + '/getUser', {
+				method: 'GET',
+				credentials: 'include'
+			});
+			if (res.status != 200) {
+				fetch(PUBLIC_API_URL + '/logout', { method: 'POST', credentials: 'include' });
+				redirect(301, '/login');
+			}
+			let userData = await res.json();
+			userData = userData['data'];
+			return {
+				user: userData
+			};
+		} catch (e) {
+			console.error(e);
 			redirect(301, '/login');
 		}
-		let userData = await res.json();
-		userData = userData['data'];
-		return {
-			user: userData
-		};
 	}
 };
--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -3,21 +3,26 @@ import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ fetch }) => {
-	const res = await fetch(PUBLIC_API_URL + '/getUser', { method: 'GET', credentials: 'include' });
-	if (res.status != 200) {
-		fetch(PUBLIC_API_URL + '/logout', { method: 'POST', credentials: 'include' });
-		redirect(301, '/login');
-	}
-	let userData = await res.json();
-	userData = userData['data'];
-	if (userData['user_type'] == 0) {
-		throw redirect(307, '/student');
-	} else if (userData['user_type'] == 1) {
-		throw redirect(307, '/mess');
-	} else if (userData['user_type'] == 2) {
-		throw redirect(307, '/admin');
-	} else {
-		fetch(PUBLIC_API_URL + '/logout', { method: 'POST', credentials: 'include' });
+	try {
+		const res = await fetch(PUBLIC_API_URL + '/getUser', { method: 'GET', credentials: 'include' });
+		if (res.status != 200) {
+			fetch(PUBLIC_API_URL + '/logout', { method: 'POST', credentials: 'include' });
+			redirect(301, '/login');
+		}
+		let userData = await res.json();
+		userData = userData['data'];
+		if (userData['user_type'] == 0) {
+			throw redirect(307, '/student');
+		} else if (userData['user_type'] == 1) {
+			throw redirect(307, '/mess');
+		} else if (userData['user_type'] == 2) {
+			throw redirect(307, '/admin');
+		} else {
+			fetch(PUBLIC_API_URL + '/logout', { method: 'POST', credentials: 'include' });
+			redirect(301, '/login');
+		}
+	} catch (e) {
+		console.error(e);
 		redirect(301, '/login');
 	}
 };
