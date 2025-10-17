fa6e80171e295e293941254f0511dbb53cb58605
Dhiraj Baid <61199043+DHIBAID@users.noreply.github.com>
2025-10-17 19:22:25 +0530

    fix: remove omitempty from VegMessCapacity and add to Name and Email in SwapRequest

--- backend/models/swapRequest.go
+++ backend/models/swapRequest.go
@@ -3,8 +3,8 @@ package models
 import "time"
 
 type SwapRequest struct {
-	Name      string    `json:"name"`
-	Email     string    `json:"email"`
+	Name      string    `json:"name,omitempty"`
+	Email     string    `json:"email,omitempty"`
 	Type      string    `json:"type" validate:"required,oneof='friend' 'public'"`
 	Password  string    `json:"password" validate:"required,min=6,max=100"`
 	UserID    uint      `json:"-" gorm:"foreignKey"`
