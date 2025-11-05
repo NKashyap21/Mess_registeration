dd86f7cdd180b1116700f4d082924ed5b60f5dc2
Dhiraj Baid <61199043+DHIBAID@users.noreply.github.com>
2025-11-05 23:01:00 +0530

    fix: Add LogsController struct to manage logging service

--- backend/handlers/logs/init.go
+++ backend/handlers/logs/init.go
@@ -2,6 +2,10 @@ package logs
 
 import "github.com/LambdaIITH/mess_registration/services"
 
+type LogsController struct {
+	loggerService *services.LoggerService
+}
+
 // InitLogsController initializes and returns a new LogsController
 func InitLogsController() *LogsController {
 	return &LogsController{
